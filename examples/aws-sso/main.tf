a9f7b64d7f72fb698f454edbc5d6b9ef5314540a
CyberViking <stevensmith529@gmail.com>
2023-09-27 12:11:19 -0700

    Create main.tf

---
+++ examples/aws-sso/main.tf
@@ -0,0 +1,37 @@
+module "sso" {
+  source = "../../aws-sso" 
+  roles = {
+    SecurityAdmin = {
+      description      = "Provides full access to AWS services and resources.",
+      session_duration = "PT2H",
+      managed_policies = [
+        "arn:aws:iam::aws:policy/AdministratorAccess",
+        "arn:aws:iam::aws:policy/ReadOnlyAccess"
+        ]
+      group = "aws-admin"
+      assignments = [
+        "123456789123", 
+        "987654321987"
+        ]
+      inline_policy = data.aws_iam_policy_document.security-admin-policy.json
+    }
+}
+
+# data "aws_iam_policy_document" "EKSAdmin" {
+#   statement {
+#     sid       = "AllowEKS"
+#     actions   = ["eks:*"]
+#     resources = ["*"]
+#   }
+#   statement {
+#     sid       = "AllowPassRole"
+#     actions   = ["iam:PassRole"]
+#     resources = ["*"]
+#     condition {
+#       test     = "StringEquals"
+#       variable = "iam:PassedToService"
+#       values   = ["eks.amazonaws.com"]
+#     }
+#   }
+# }
+}
