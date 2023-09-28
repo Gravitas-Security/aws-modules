00781547e7286fb282817b51cb4701375390cd8a
cyberviking949 <stevensmith529@gmail.com>
2023-09-27 18:53:45 -0700

    cleanup

--- examples/aws-sso/main.tf
+++ examples/aws-sso/main.tf
@@ -5,33 +5,14 @@ module "sso" {
       description      = "Provides full access to AWS services and resources.",
       session_duration = "PT2H",
       managed_policies = [
-        "arn:aws:iam::aws:policy/AdministratorAccess",
-        "arn:aws:iam::aws:policy/ReadOnlyAccess"
+        "arn:aws:iam::aws:policy/AdministratorAccess"
         ]
       group = "aws-admin"
       assignments = [
-        "808696518247", 
-        "047640678298"
+        "123456789123", 
+        "987654321987"
         ]
       inline_policy = data.aws_iam_policy_document.security-admin-policy.json
     }
 }
-
-# data "aws_iam_policy_document" "EKSAdmin" {
-#   statement {
-#     sid       = "AllowEKS"
-#     actions   = ["eks:*"]
-#     resources = ["*"]
-#   }
-#   statement {
-#     sid       = "AllowPassRole"
-#     actions   = ["iam:PassRole"]
-#     resources = ["*"]
-#     condition {
-#       test     = "StringEquals"
-#       variable = "iam:PassedToService"
-#       values   = ["eks.amazonaws.com"]
-#     }
-#   }
-# }
 }
