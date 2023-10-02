01261b18ffc69c797ee879dadd0e753754ab0f79
cyberviking949 <stevensmith529@gmail.com>
2023-10-02 05:04:00 -0700

    stuff

--- aws-scp/providers.tf
+++ aws-scp/providers.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.18.1"
     }
   }
-  cloud {
-    organization = "gravitas-security"
-    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
-
-    workspaces {
-      tags = ["aws-infra-scps", "source:cli"]
-    }
-  }
 }
 
 provider "aws" {
