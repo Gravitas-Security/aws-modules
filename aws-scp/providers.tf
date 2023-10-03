0f6f11814565a8e25cd850933deb51c07ce66968
cyberviking949 <stevensmith529@gmail.com>
2023-10-03 07:53:31 -0700

    feature: adding in cloud provider

--- aws-scp/providers.tf
+++ aws-scp/providers.tf
@@ -5,7 +5,16 @@ terraform {
       version = "~> 5.18.1"
     }
   }
+  cloud {
+    organization = "gravitas-security"
+    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+
+    workspaces {
+      tags = ["aws-infra-scps", "source:cli"]
+    }
+  }
 }
+
 
 provider "aws" {
   region   = "us-west-2"
