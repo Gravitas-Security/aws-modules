6f4b1324599565456bb8e9868e53f39470a39c75
cyberviking949 <stevensmith529@gmail.com>
2023-10-06 12:58:00 -0700

    feat: adding k8s access policy as a attribute bool

---
+++ aws-sso/k8s_access.tf
@@ -0,0 +1,15 @@
+data "aws_iam_policy_document" "eks_access" {
+    statement {
+    sid = "eksAccessPolicy"
+
+    actions = [
+        "eks:DescribeCluster",
+        "eks:ListClusters"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+ 
+}
