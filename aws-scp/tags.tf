5768f9892dd337409c7e9937731bb2a95e333073
cyberviking949 <stevensmith529@gmail.com>
2023-10-13 03:42:12 -0700

    feat: moving tags to downstream

--- aws-scp/tags.tf
+++ aws-scp/tags.tf
@@ -2,9 +2,6 @@ variable "defaultTags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
   default = {
-    owner       = "something"
-    cost-centre = "something"
-    contact     = "something"
     tf_Managed  = "true"
     tf_module   = "github.com/cyberviking949/aws-modules/aws-scp"
   }
