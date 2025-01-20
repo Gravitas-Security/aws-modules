6e2da9e95df38e7b014fc9d9c4603623c55dc9d8
Steven Smith <stevensmith529@gmail.com>
2025-01-20 13:59:13 +0530

    feat: updating providers
    adding new aws provider version and adding enabling declaraitive policy support

--- aws-org/providers.tf
+++ aws-org/providers.tf
@@ -2,15 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.78.0"
-    }
-  }
-  cloud {
-    organization = "gravitas-security"
-    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
-
-    workspaces {
-      tags = ["aws-infra-org", "source:cli"]
+      version = "~> 5.84.0"
     }
   }
 }
